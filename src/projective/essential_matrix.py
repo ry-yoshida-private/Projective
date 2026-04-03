b49a6c132e9f48461fb7397219235f65e280e870
ry-yoshida <ry-yoshida@yachiyo-eng.co.jp>
2026-04-03 06:59:31 +0000

    update essential_matrix

--- src/projective/essential_matrix.py
+++ src/projective/essential_matrix.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import cv2
 import numpy as np
 from dataclasses import dataclass
 
@@ -18,4 +19,52 @@ class EssentialMatrix:
     def __post_init__(self):
         if self.value.shape != (3, 3):
             raise ValueError("Essential matrix must be a 3x3 matrix")
+
+    @classmethod
+    def from_points(
+        cls,
+        points1: np.ndarray,
+        points2: np.ndarray,
+        intrinsic_matrix: np.ndarray,
+        prob: float = 0.999,
+        threshold: float = 1.0
+        ) -> tuple[EssentialMatrix, np.ndarray]:
+        """
+        Create an essential matrix from a set of points and an intrinsic matrix.
+
+        Parameters
+        ----------
+        points1: np.ndarray
+            Points in the first image.
+        points2: np.ndarray
+            Points in the second image.
+        intrinsic_matrix: np.ndarray
+            Intrinsic matrix.
+        prob: float
+            Probability of the essential matrix.
+        threshold: float
+            Threshold of the essential matrix.
+
+        Returns
+        -------
+        tuple[EssentialMatrix, np.ndarray]:
+            - EssentialMatrix: The estimated essential matrix.
+            - np.ndarray: The mask of the estimated essential matrix.
+        """
+        if points1.shape != points2.shape:
+            raise ValueError("Points1 and points2 must have the same shape")
+        if points1.shape[0] < 5:
+            raise ValueError("At least 5 points are required to estimate an essential matrix")
+        if intrinsic_matrix.shape != (3, 3):
+            raise ValueError("Intrinsic matrix must be a 3x3 matrix")
+
+        E, e_mask = cv2.findEssentialMat(
+            points1, 
+            points2, 
+            intrinsic_matrix, 
+            method=cv2.RANSAC, 
+            prob=prob, 
+            threshold=threshold
+            )
+        return cls(value=E), e_mask
 
